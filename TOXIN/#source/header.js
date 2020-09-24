cf36a46fdf26a0b30cdc9ec498e781afa2b20535
Anton Mashchenko <anjiilop25@gmail.com>
2020-09-24 16:33:58 +0300

    header and main_landing page

--- TOXIN/#source/header.js
+++ TOXIN/#source/header.js
@@ -4,45 +4,43 @@ $(document).ready(function(){
 	$('body').toggleClass('lock');
 	});
 
-		
+	// 	$('.menu__link-about').click(function(event) {
+	// 	$('.menu__link-about').addClass('active');
+	// 	$('.menu__link-services').removeClass('active');
+	// 	$('.menu__link-vacancies').removeClass('active');
+	// 	$('.menu__link-news').removeClass('active');
+	// 	$('.menu__link-agree').removeClass('active');
+	// });
 
-		$('.menu__link-about').click(function(event) {
-		$('.menu__link-about').addClass('active');
-		$('.menu__link-services').removeClass('active');
-		$('.menu__link-vacancies').removeClass('active');
-		$('.menu__link-news').removeClass('active');
-		$('.menu__link-agree').removeClass('active');
-	});
+	// 	$('.menu__link-services').click(function(event) {
+	// 	$('.menu__link-about').removeClass('active');
+	// 	$('.menu__link-services').addClass('active');
+	// 	$('.menu__link-vacancies').removeClass('active');
+	// 	$('.menu__link-news').removeClass('active');
+	// 	$('.menu__link-agree').removeClass('active');
+	// });
 
-		$('.menu__link-services').click(function(event) {
-		$('.menu__link-about').removeClass('active');
-		$('.menu__link-services').addClass('active');
-		$('.menu__link-vacancies').removeClass('active');
-		$('.menu__link-news').removeClass('active');
-		$('.menu__link-agree').removeClass('active');
-	});
+	// 	$('.menu__link-vacancies').click(function(event) {
+	// 	$('.menu__link-about').removeClass('active');
+	// 	$('.menu__link-services').removeClass('active');
+	// 	$('.menu__link-vacancies').addClass('active');
+	// 	$('.menu__link-news').removeClass('active');
+	// 	$('.menu__link-agree').removeClass('active');
+	// });
 
-		$('.menu__link-vacancies').click(function(event) {
-		$('.menu__link-about').removeClass('active');
-		$('.menu__link-services').removeClass('active');
-		$('.menu__link-vacancies').addClass('active');
-		$('.menu__link-news').removeClass('active');
-		$('.menu__link-agree').removeClass('active');
-	});
+	// 	$('.menu__link-news').click(function(event) {
+	// 	$('.menu__link-about').removeClass('active');
+	// 	$('.menu__link-services').removeClass('active');
+	// 	$('.menu__link-vacancies').removeClass('active');
+	// 	$('.menu__link-news').addClass('active');
+	// 	$('.menu__link-agree').removeClass('active');
+	// });
 
-		$('.menu__link-news').click(function(event) {
-		$('.menu__link-about').removeClass('active');
-		$('.menu__link-services').removeClass('active');
-		$('.menu__link-vacancies').removeClass('active');
-		$('.menu__link-news').addClass('active');
-		$('.menu__link-agree').removeClass('active');
-	});
-
-		$('.menu__link-agree').click(function(event) {
-		$('.menu__link-about').removeClass('active');
-		$('.menu__link-services').removeClass('active');
-		$('.menu__link-vacancies').removeClass('active');
-		$('.menu__link-news').removeClass('active');
-		$('.menu__link-agree').addClass('active');
-	});
+	// 	$('.menu__link-agree').click(function(event) {
+	// 	$('.menu__link-about').removeClass('active');
+	// 	$('.menu__link-services').removeClass('active');
+	// 	$('.menu__link-vacancies').removeClass('active');
+	// 	$('.menu__link-news').removeClass('active');
+	// 	$('.menu__link-agree').addClass('active');
+	// });
 });
